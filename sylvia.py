ba978a255cdc81f0d77ca97587bdf811f6ee5175
Sylvia Reviar <129020946+SylviaReviar@users.noreply.github.com>
2025-01-29 12:57:19 -0600

    Added Sylvia's character profile as a test.

---
+++ sylvia.py
@@ -0,0 +1,17 @@
+# Character Profile
+
+# Name
+name = "Sylvia Reviar"
+print("Name:", name)
+
+# Age
+y = 10
+print("Age:", y)
+
+# Level
+x = 5
+print("Level:", x)
+
+# Type
+type = "Fairy"
+print("Type:", type)
