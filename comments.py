c56a3e5db788cd8c97ad1daddbee1a562481befc
Sylvia Reviar <129020946+SylviaReviar@users.noreply.github.com>
2025-02-03 12:45:12 -0600

    Added PE1 2.5: Comments
    2/3/2025

---
+++ comments.py
@@ -0,0 +1,12 @@
+"""
+    Use a # to create comments on individual lines
+    And use quotations to create comments lasting multiple lines.
+    """
+
+tank_size = 15
+# If the car you're driving can hold 15 gallons...
+distance = 600
+# And you drive 600 miles...
+mpg = distance / tank_size
+# Then how many miles can you go for every gallon of gas?
+print("Miles per gallon:", mpg)
