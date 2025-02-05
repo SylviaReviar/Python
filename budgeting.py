07f1dd3e9bc999778dad0db111f91218fe25ff9c
Sylvia Reviar <sonyarosa02@gmail.com>
2025-02-05 12:14:39 -0600

    Updated budgeting code

--- budgeting.py
+++ budgeting.py
@@ -15,17 +15,11 @@
 # Let's interact with the user
 
 budget = float(input("Please enter your monthly income for the budget: "))
+
+# VARIABLES
+
 housing = float(input(
     "Please enter your housing costs (rent or mortgage costs plus HOA if you have it): "))
-
-# Let's set up the calculations...
-housing_percent = housing/budget * 100
-
-# And now for the output!
-print(f"Your housing is {housing_percent:.1f}% of your monthly budget.")
-
-# Let's add more variables.
-
 utilities = float(input("Please enter the cost of utilities: "))
 groceries = float(input("Please enter the cost of groceries: "))
 transportation = float(input("Please enter the cost of transportation: "))
@@ -36,6 +30,7 @@ debt = float(input("Please enter the cost of debt: "))
 
 # CALCULATIONS
 
+housing_percent = housing/budget * 100
 utility_percent = utilities/budget * 100
 groceries_percent = groceries/budget * 100
 trans_percent = transportation/budget * 100
@@ -46,6 +41,7 @@ debt_percent = debt/budget * 100
 
 # OUTPUTS
 
+print(f"Your housing is {housing_percent:.1f}% of your monthly budget.")
 print(f"Your utilities are {utility_percent:.1f}% of your budget.")
 print(f"Your groceries are {groceries_percent:.1f}% of your budget.")
 print(f"Your transportation is {trans_percent:.1f}% of your budget.")
