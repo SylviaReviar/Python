ed11a31f0e5cf77d5ee4c3cd1b19e4d22dc0753d
Sylvia Reviar <sonyarosa02@gmail.com>
2025-02-05 13:22:12 -0600

    Made it more legible oops

--- ughmath.py
+++ ughmath.py
@@ -2,35 +2,35 @@
         Operators
     """
 
-print("4 + 4 =", 4 + 4)
-print("Sylvia " + "Reviar")
-print("4 + 4 as strings", "4" + "4")
+# print("4 + 4 =", 4 + 4)
+# print("Sylvia " + "Reviar")
+# print("4 + 4 as strings", "4" + "4")
 
-# More Math Functions
+# # More Math Functions
 
-print("Addition")
-print("12 + 8 =", 12 + 8)
+# print("Addition")
+# print("12 + 8 =", 12 + 8)
 
-print("Subtraction")
-print("20 - 8 =", 20 - 8)
+# print("Subtraction")
+# print("20 - 8 =", 20 - 8)
 
-print("Multiplication")
-print("4 * 5 =", 4 * 5)
+# print("Multiplication")
+# print("4 * 5 =", 4 * 5)
 
-print("Division")
-print("20 / 5 =", 20 / 5)
+# print("Division")
+# print("20 / 5 =", 20 / 5)
 
-print("Integer Division")
-print("33 // 4 =", 33 // 4)
+# print("Integer Division")
+# print("33 // 4 =", 33 // 4)
 
-print("Modulus")
-print("33 % 4 =", 33 % 4)
+# print("Modulus")
+# print("33 % 4 =", 33 % 4)
 
-print("Exponentiation")
-print("3 ** 3 =", 3 ** 3)
+# print("Exponentiation")
+# print("3 ** 3 =", 3 ** 3)
 
-print("PEMDAS")
-print("5 + 4 * 7 ** 2 =", 5 + 4 * 7 ** 2)
+# print("PEMDAS")
+# print("5 + 4 * 7 ** 2 =", 5 + 4 * 7 ** 2)
 
 # Variable Math
 
