497e11fbf80ce66cd4c6f0611f37abc62eebe120
Sylvia Reviar <sonyarosa02@gmail.com>
2025-02-17 14:36:44 -0600

    homework

---
+++ divisible_by_7.py
@@ -0,0 +1,21 @@
+"""
+    Write a Python program to find and print all numbers:
+    - Divisible by 7
+    - Between 1 and 300
+
+    Use a for loop and modulus (%) operator
+
+    oh god, okay... So for starters, modulus means finding a remainder.
+
+    for loop:
+        A control flow statement for iteration, allowing repeated execution of a block of code.
+
+    range():
+        A build-in Python function for generating a sequence of numbers.
+
+    break:
+        Break the loop.
+
+    continue:
+        Skip the rest of the current iteration and move on to the next.
+    """
