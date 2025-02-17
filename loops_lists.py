4f573142c25dd8a1b2338e5b3506d0a08a8e0d7a
Sylvia Reviar <sonyarosa02@gmail.com>
2025-02-17 12:25:04 -0600

    notes for feb 17

---
+++ loops_lists.py
@@ -0,0 +1,24 @@
+"""
+        While loops
+        For loops
+        Lists
+    """
+
+# Flag demo
+# entry = "hi"
+# while entry != "done":
+#     entry = input("How are you today? Enter \"done\" when finished. ")
+#     if entry != "done":
+#         print("It is one of those days, isn't it?")
+
+# number = 10
+# while number >= 0:
+#     print(number)
+#     number -= 1  # same as number = number - 1
+
+# for num in range(10, -1, -1):
+#     if num == 5:
+#         continue
+#     if num == 3:
+#         break
+#     print(num)
