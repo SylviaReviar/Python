26223965837308719513946ee30400498b65716c
Sylvia Reviar <sonyarosa02@gmail.com>
2025-02-24 14:49:33 -0600

    i think i just wrote a whole fanfiction

---
+++ stardust_accelerator.py
@@ -0,0 +1,128 @@
+"""
+    Playtime with functions :D
+
+    Yu-Gi-Oh! 5D's Mad-Libs!
+
+    (If you can't think of people to input to test the story, then go ahead and use fictional characters)
+    And yes, this is named (and written) after Yu-Gi-Oh! World Championships 2009: Stardust Accelerator.
+    Yes, I'm a huge nerd. And yes, this is the weirdest thing I've submitted to a professor.
+    I am describing an anime to you. The concept is not mine. It's bizarre and takes itself way too seriously.
+    And I think that's funny.
+    """
+
+
+def main():
+    player = input("What's your name?: ")
+    yusei = input("Who is your best friend?: ")
+    rally = input("Who is your little sibling?: ")
+    tank = input("Who is one of your other friends?: ")
+    blitz = input("Who is another one of your friends?: ")
+    nervin = input("Who is a third one of your friends?: ")
+    vehicle = input("What is your vehicle?: ")
+    deck = input("What deck do you use?: ")
+    satellite = input("What is the city you live in?: ")
+    new_domino = input("What is the city next to you?: ")
+    trudge = input("Who is someone you hate?: ")
+    goodwin = input("Who is someone you hate even more?: ")
+    leo = input("Name a twin: ")
+    luna = input("Name another twin: ")
+    akiza = input("Name someone you're scared of: ")
+    jack = input("Name your rival: ")
+
+    yugioh5Ds(player, yusei, rally, tank, blitz, nervin, vehicle, deck,
+              satellite, new_domino, trudge, goodwin, leo, luna, akiza, jack)
+
+
+def yugioh5Ds(player, yusei, rally, tank, blitz, nervin, vehicle, deck, satellite, new_domino, trudge, goodwin, leo, luna, akiza, jack):
+    print(f"\n{player} had no memories of their own. All they knew was pain.")
+    print("They were woken up by the cold scent of rain mixing with concrete, shaken awake by someone's small hands.")
+    print(
+        f"The child was named {rally}. They looked concerned upon seeing their injuries, and took them home.")
+    print(f"{rally} and their friends lived in a ruined city called {satellite}. One by one, they introduced {player}.")
+    print(
+        f"The headstrong one was {blitz}. The clueless one was {tank}. And the anxious one was {nervin}.")
+    print(
+        f"\nThe three asked {player} about their lost memories. All they had on them was their deck.")
+    print(f"{rally} challenged {player} to a duel to try and restore their memories. Using their {deck}, {player} won.")
+    print("The others were in awe. Being a skilled duelist was apparently important around these parts.")
+    print(
+        f"After the duel, the sound of a {vehicle} echoed across the streets. It was the group leader, {yusei}.")
+    print(f"{yusei} was quiet, but determined. They had a mission, and hardly paid attention to {player}.")
+    print(
+        f"So long as they weren't bothering anyone, {yusei} wouldn't bother them.")
+    print(
+        f"\nTime passed in {satellite}. The more {player} spent their time here, the more they learned.")
+    print(
+        "Duels could be had in one of two ways: traditionally standing, or Turbo Duels.")
+    print(
+        f"It seemed crazy, and even a little stupid to them, that card games were being played while driving {vehicle}s...")
+    print("But there was nothing they could really do about that.")
+    print(
+        f"Especially because they seemed to secretly be as capable of playing them as {yusei}.")
+    print(
+        f"\nNoticing {player}'s adept skills, {rally}, {blitz}, {tank}, and {nervin} all worked together to build them a {vehicle} of their own.")
+    print(
+        f"When they were done, {player} had a suitable vessel to duel on.")
+    print(
+        f"\nBecause of their skill in Turbo Duels, {player}'s friends believed them to be from the next city over, {new_domino}.")
+    print(
+        f"Coincidentally, {yusei} was heading there, too. He had business with the famous Turbo Duel King, {jack}.")
+    print(
+        f"So {player} and {yusei} decided to go to {new_domino} together, to kill two birds with one stone.")
+    print(f"To recover {player}'s memories and {yusei}'s prized card.")
+    print(
+        f"\nUnfortunately, Turbo Dueling, possessing cards, and any other quality of life improvements were all outlawed in {satellite}.")
+    print(f"As {yusei} and {player} drove off to {new_domino}, Officer {trudge} chased after them with sirens blaring.")
+    print(
+        f"The chase was mighty, and {trudge} even caught the two in a Turbo Duel.")
+    print(
+        f"But after cleverly splitting apart and working together, {yusei} and {player} shook Officer {trudge} off their tail and made it to {new_domino}.")
+    print(
+        f"\nWaiting for them at the other side was {jack}, ready to take on {yusei} in a secret Turbo Duel.")
+    print(f"{player} watched as the two fought so hard, they summoned a giant crimson dragon and blacked out the entire city.")
+    print(
+        f"Amidst the blackout, however, {player} was kidnapped, and {yusei} was arrested.")
+    print("\nEnter the Arcadia Movement.")
+    print(f"\n{player} awoke once more, memories scrambled. They were now a member of a psychic organization.")
+    print(
+        f"The top amongst their ranks was {akiza}, whom {player} was determined to befriend.")
+    print(
+        f"They were hurt beyond recognition. {akiza} was a psychic duelist whose power was so immense, they couldn't control it.")
+    print("It was bad enough that they hurt others without meaning to.")
+    print(f"{player} stayed strong with them, though. {akiza} was all alone, after all. They needed a friend.")
+    print(
+        f"\nWhile they stayed in the Arcadia Movement, {player} also had odd dreams.")
+    print(
+        f"In them, a small child named {luna} appeared, curiously asking who they were.")
+    print(
+        f"For some reason, {luna} felt familiar to {player}. They trusted them without a second thought.")
+    print(
+        f"\nWeeks passed. {yusei} was still imprisoned, with no way to contact {player}.")
+    print(
+        f"They worried about their friend. {new_domino} Prison was unforgiving.")
+    print(f"But finally, {yusei} dueled their way to freedom, thanks to the intervention of Director {goodwin}, the Public Security Bureau head.")
+    print(f"Officer {trudge} wouldn't give it up. When {yusei} was freed, they went straight for the Impound Center for their {vehicle} and deck.")
+    print(
+        f"After narrowly escaping a second (unauthorized) arrest, {yusei} found themself in the apartment of a pair of twins.")
+    print(
+        f"The energetic {leo} begged {yusei} for a duel, and they acquiesced as thanks for giving them a place to stay for the night.")
+    print(f"The more reserved {luna}, however, preferred to just watch.")
+    print(
+        f"While they were at the twins' house, {yusei} learned about the Fortune Cup, a Dueling tournament {luna} was invited to.")
+    print(f"{leo} was jealous, because they liked dueling more than their twin did. But the invitation had {luna}'s name on it.")
+    print(f"{leo} planned to dress up as {luna} and participate in their stead, since they didn't even want to.")
+    print(f"\n{yusei} left a gift for the twins and left the next morning. They couldn't endanger them. They were too young.")
+    print(
+        f"On their way out, {yusei} received an invitation of their own from a goon of {goodwin}'s.")
+    print(
+        f"It was an ultimatum. Duel in the Fortune Cup, or {rally}, {blitz}, {tank}, and {nervin} would pay the price.")
+    print(
+        f"Unbeknownst to {yusei}, {player} and {akiza} had also received invitations.")
+    print(
+        f"Those who won in the finals would have a chance to duel against {jack}.")
+    print(f"{yusei} intended to win. They had a score to settle, and friends to save.")
+    print(
+        f"\nThe Fortune Cup, with {player}, {yusei}, {jack}, {akiza}, {leo}, and {luna} as participants, was now under way.")
+
+
+main()
