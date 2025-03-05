df53af7aae89aead268c73a42a364e397b06686a
Sylvia Reviar <sonyarosa02@gmail.com>
2025-03-05 13:52:03 -0600

    finish homework at home

---
+++ bmi_sucks.py
@@ -0,0 +1,22 @@
+"""
+    As we all know, BMI is THE most accurate way of calculating obesity.
+    That is, if you ignore any kind of muscle mass, health conditions, body types...
+    In other words, it's society's way of bullying people!
+    Hooray!!!
+
+    Side note: I'm obese according to my BMI calculations, so do with that information what you will.
+    As we all know, I am the textbook definition of obese.
+
+    Sorry, I'll cut the sarcasm now. Here's the assignment:
+    The user will enter their weight in pounds(lbs) and their height in inches(in).
+    The program's job is to calculate lbs into kg and in into m.
+
+    1 lb = 0.453592 kg
+    1 in = 0.0254 m
+
+    Then, using the BMI formula, we calculate what ranges they fall into.
+
+    bmi = weight / (height ** 2)
+
+    Oh, and this is a scope assignment. Time for me to figure out what the heck that means.
+    """
