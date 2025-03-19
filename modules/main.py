40489e472f3316f3173c4f167534d1f623a88f6b
Sylvia Reviar <sonyarosa02@gmail.com>
2025-03-19 13:45:12 -0500

    calculator

---
+++ modules/main.py
@@ -0,0 +1,26 @@
+"""
+    more following along
+
+    calculator
+    """
+
+import math_operations.calculator as calculator
+
+result = calculator.add(5, 3)
+print(f"The answer is: {result}")
+
+result = calculator.subtract(10, 4)
+print(f"The answer is: {result}")
+
+"""
+more notes yay
+
+....wait this was actual homework???
+
+I didn't realize.
+
+Also I moved this from my __init__.py file bc apparently it was supposed to be empty??? Anyway, I don't think I did this right????
+"""
+
+result = calculator.add(12, 4)
+print(f"The answer is: {result}")
---
+++ modules/math_operations/geometry.py
@@ -0,0 +1,5 @@
+"""
+    Just to double check I understand this right???
+
+    I'm gonna make a module of some geometrical formulas and try to import them.
+    """
