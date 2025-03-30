e45dd4b2abe1d3577c5c507126997669f939dce7
Sonya Rozenfeld <sonyarosa02@gmail.com>
2025-03-30 16:08:41 -0500

    oof

---
+++ strings.py
@@ -0,0 +1,36 @@
+"""
+    oh god spring break is over as of tomorrow. Sorry it took me so long to get to this...
+    I wore myself down trying to rush on my game development project, beating myself up and drawing
+    over and over like my life depended on it
+    ended up giving myself HUUUUUGE headaches...
+    and i crashed out HARD. even now, I'm fighting through another headache to try and get this done.
+
+    The assignment says to complete the code given to us by Meri.
+    Where it says T O D O: I have to follow the instructions. (I spaced them out because they changed color)
+    (I didn't want to change the code by accident by having something in my docstring...)
+
+    here goes everything I guess
+
+    ...never mind my brain isn't working. I'm waiting til tomorrow...
+    """
+
+
+def main():
+
+    # Example string
+    example_string = "Hello, Python programmers!"
+    # TODO: Iterate through the string and print each character
+    print("Iterating through the string:")
+    # TODO: Find and print the character with the minimum ASCII value in the string
+    print("\nCharacter with the minimum ASCII value:")
+    # TODO: Find and print the character with the maximum ASCII value in the string
+    print("\nCharacter with the maximum ASCII value:")
+    # TODO: Find and print the index of the first occurrence of 'o' in the string
+    print("\nIndex of 'o':")
+    # TODO: Convert the string into a list of characters and print the list
+    print("\nConverting string to a list of characters:")
+    # TODO: Count and print the number of occurrences of 'o' in the string
+    print("\nCount of 'o' in the string:")
+
+
+main()
