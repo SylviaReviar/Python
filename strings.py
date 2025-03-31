45086d8e4c884ff3da3e7eeded733427308ea2e3
Sylvia Reviar <sonyarosa02@gmail.com>
2025-03-31 12:49:45 -0500

    homework complete

--- strings.py
+++ strings.py
@@ -6,12 +6,15 @@
     and i crashed out HARD. even now, I'm fighting through another headache to try and get this done.
 
     The assignment says to complete the code given to us by Meri.
-    Where it says T O D O: I have to follow the instructions. (I spaced them out because they changed color)
-    (I didn't want to change the code by accident by having something in my docstring...)
+    Where it says TODO: I have to follow the instructions.
 
     here goes everything I guess
 
     ...never mind my brain isn't working. I'm waiting til tomorrow...
+    
+    3/31/25: Ok I'm back. Let's take a crack at this now...
+
+    Wow that was surprisingly simple and fast. I feel silly for getting a headache over this yesterday...
     """
 
 
@@ -20,17 +23,19 @@ def main():
     # Example string
     example_string = "Hello, Python programmers!"
     # TODO: Iterate through the string and print each character
-    print("Iterating through the string:")
+    for i in example_string:
+        print(f"Iterating through the string: {i}")
     # TODO: Find and print the character with the minimum ASCII value in the string
-    print("\nCharacter with the minimum ASCII value:")
+    print(f"\nCharacter with the minimum ASCII value: {min(example_string)}")
     # TODO: Find and print the character with the maximum ASCII value in the string
-    print("\nCharacter with the maximum ASCII value:")
+    print(f"\nCharacter with the maximum ASCII value: {max(example_string)}")
     # TODO: Find and print the index of the first occurrence of 'o' in the string
-    print("\nIndex of 'o':")
+    print(f"\nIndex of 'o': {example_string.index("o")}")
     # TODO: Convert the string into a list of characters and print the list
-    print("\nConverting string to a list of characters:")
+    list_of_string = list(example_string)
+    print(f"\nConverting string to a list of characters: {list_of_string}")
     # TODO: Count and print the number of occurrences of 'o' in the string
-    print("\nCount of 'o' in the string:")
+    print(f"\nCount of 'o' in the string: {example_string.count("o")}")
 
 
 main()
